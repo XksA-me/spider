2744b8b535d0d8675f3e5429f7ce54adfd6dc875
极简XksA <43670614+XksA-me@users.noreply.github.com>
2018-10-18 17:47:54 +0800

    更新，修改错误
    更新，修改错误

--- spider_maoyan/main_page.py
+++ spider_maoyan/main_page.py
@@ -7,14 +7,16 @@ goal : 爬取猫眼《悲伤逆流成河》影评，词云可视化
 # 猫眼电影介绍url
 # http://maoyan.com/films/1217236
 
-import requests,time
-from fake_useragent import UserAgent
+import requests,time,random
+# from fake_useragent import UserAgent
 import json,csv,os
 import pandas as pd
 
+USER_AGENTS = [ "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)", "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)", "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)", "Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)", "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 2.0.50727; Media Center PC 6.0)", "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0; WOW64; Trident/4.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET CLR 1.0.3705; .NET CLR 1.1.4322)", "Mozilla/4.0 (compatible; MSIE 7.0b; Windows NT 5.2; .NET CLR 1.1.4322; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 3.0.04506.30)", "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN) AppleWebKit/523.15 (KHTML, like Gecko, Safari/419.3) Arora/0.3 (Change: 287 c9dfb30)", "Mozilla/5.0 (X11; U; Linux; en-US) AppleWebKit/527+ (KHTML, like Gecko, Safari/419.3) Arora/0.6", "Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.2pre) Gecko/20070215 K-Ninja/2.1.1", "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9) Gecko/20080705 Firefox/3.0 Kapiko/3.0", "Mozilla/5.0 (X11; Linux i686; U;) Gecko/20070322 Kazehakase/0.4.5", "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.8) Gecko Fedora/1.9.0.8-1.fc10 Kazehakase/0.5.6", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20", "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52", ]
+
 class Spidermaoyan():
 	headers = {
-		    "User-Agent": UserAgent(verify_ssl=False).random,
+		    "User-Agent": random.choice(USER_AGENTS),
 		    "Host":"m.maoyan.com",
 		    "Referer":"http://m.maoyan.com/movie/1217236/comments?_v_=yes"
 		}
@@ -22,6 +24,7 @@ class Spidermaoyan():
 	def __init__(self,url,time):
 		self.url = url
 		self.time = time
+		
 	# 发送get请求
 	def get_json(self):
 		# 发送get请求
@@ -48,6 +51,7 @@ class Spidermaoyan():
 			score = data["score"]
 			list_one = [self.time,nickName,gender,cityName,userLevel,score,content]
 			list_info.append(list_one)
+		print(list_info)
 		self.file_do(list_info)
 	
 	# 存储文件
@@ -83,7 +87,7 @@ def spider_maoyan():
 		if json_comment["total"] == 0: # 当前时间内评论爬取完成
 			if j < 9:  # 九月份(9天)
 				startTime = '2018-09-%d'%day[j]
-			elif j>= 9 and j < 15:  # 十月份(6天)
+			elif j >= 9 and j < 15:  # 十月份(6天)
 				startTime = '2018-10-%d'%day[j]
 			else: # 全部爬完
 				break
@@ -95,6 +99,6 @@ def spider_maoyan():
 		# time.sleep(1)
 	
 
-if __name__ == 'main':
+if __name__ == '__main__':
 	spider_maoyan()
 	
